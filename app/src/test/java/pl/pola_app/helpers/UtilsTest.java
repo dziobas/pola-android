063547ab23c1b289f89c51806216eaa037f66b6e
Paweł Zięba <pawel@spotify.com>
2016-05-25 11:46:46 +0200

    Testing CI

---
+++ app/src/test/java/pl/pola_app/helpers/UtilsTest.java
@@ -0,0 +1,17 @@
+package pl.pola_app.helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilsTest {
+
+    @Test
+    public void failingTest() throws Exception {
+        Assert.fail("test fail");
+    }
+
+    @Test
+    public void successTest() throws Exception {
+        
+    }
+}
